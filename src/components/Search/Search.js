7ff6e1c9d497a57a79ff16086e506a20a7ebb9c6
Omkar <aomkar481@gmail.com>
2020-12-29 12:24:43 +0530

    search-widget completed

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -4,10 +4,11 @@ import axios from 'axios';
 const Search = () => {
 
     const [term, setTerm] = useState("");
+    const [results,setResults] = useState([]);
 
     useEffect(()=>{
         const fetchData = async () => {
-            await axios.get('https://en.wikipedia.org/w/api.php',{
+           const {data} = await axios.get('https://en.wikipedia.org/w/api.php',{
                 params:{
                     action:'query',
                     list : 'search',
@@ -15,22 +16,63 @@ const Search = () => {
                     format : 'json',
                     srsearch : term
                 }
-            })   
+            });
+
+            setResults(data.query.search);
+      
         }
 
-        fetchData();
+        const timeoutID = setTimeout(()=>{
+            if(term.length > 0)fetchData();
+        },1000);
+
+        return () => {
+            clearTimeout(timeoutID);
+        }
 
     },[term]);
 
+    const renderedResults = results.map((item,index) => {
+       
+        return (
+            <div className="row" key={item.pageid}>
+                <div className="col p-3">
+                    <div className="card">
+                            
+                        <div className="card-body">
+                
+                            <a 
+                                href={`https://en.wikipedia.org?curid=${item.pageid}`}
+                                target='_blank'
+                            >
+                                <h5 className="card-title ">
+                                    {item.title}
+                                </h5>
+                            </a>
+                            <div className="card-text">
+                            
+                                <span dangerouslySetInnerHTML={{__html:item.snippet}} ></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+        
+    })
+
     return (
         <div>
-            <div className="form-group">
+            <div className="form-group m-3">
                 <label for="searchInput">Enter Search term</label>
                 <input 
                     className="form-control" 
                     value={term}
                     onChange={(event) => setTerm(event.target.value)}
                 />
+            </div>
+            <div className="container">
+                {renderedResults}
             </div>
         </div>
     )
